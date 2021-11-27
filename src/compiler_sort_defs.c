7ffb77b421a8057f75b7f7750fe3ddd5e65356a2
Ben Ayers-Glassey <bayersglassey@gmail.com>
2021-11-26 20:27:42 -0800

    Factored compiler_sort_defs out of compiler_sort_inplace_refs... now it's generic, and we can rewrite compiler_sort_aliases to use it, and (hopefully) handle TYPE_TAG_FUNC properly.

--- src/compiler_sort_defs.c
+++ src/compiler_sort_defs.c
@@ -1,5 +1,11 @@
 
 
+/* Do you like implementing graph algorithms in C?..
+With "generics" implemented with function pointers?..
+You monster.
+Come right on in. */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,7 +14,76 @@
 
 
 
+/************** TYPEDEFS *******************/
+
+typedef struct sorter sorter_t;
+typedef struct sorted_def sorted_def_t;
+
+/* What to do when visiting a def during a traversal of a partial ordering
+of defs */
+typedef void visit_sorted_def_t(sorted_def_t *sorted_def);
+
+/* A *step* of a breadth-first traversal of a partial ordering of defs; in
+particular, this implements the logic for finding a def's children within
+the partial ordering */
+typedef void visit_sorted_def_children_t(compiler_t *compiler,
+    type_def_t *def, sorted_def_t *sorted_def,
+    sorted_def_t *sorted_defs, visit_sorted_def_t *visit_sorted_def);
+
+
+
+/************** STRUCTS *******************/
+
+struct sorter {
+
+    /* Pointer to the end of new_defs, so that we can push defs onto the
+    end of it easily, using the expression: *(--new_defs_end) = def
+    NOTE: by "end" I mean... uhhh... the start. O_o
+    But we're treating new_defs as a stack which fills up from the right.
+    So new_defs_end is the *left* side of the stack, where things are pushed.
+    See: sorted_def_sort */
+    type_def_t **new_defs_end;
+
+};
+
+struct sorted_def {
+    /* Information about a def which is being sorted */
+
+    sorter_t *sorter;
+
+    type_def_t *def;
+
+    int
+        /* non_root: quick lookup for whether this def has any
+        references to it from other defs */
+        non_root: 1,
+
+        /* sorted: quick lookup for whether this def has been sorted
+        (assigned a new_i value) */
+        sorted: 1;
+
+    /* Once this def has been sorted, new_i is the index it will have in
+    the new compiler->defs.elems */
+    size_t new_i;
+};
+
+
+
+
+/************** STATIC PROTOTYPES *******************/
+
+static visit_sorted_def_t visit_sorted_def_mark_non_root;
+static visit_sorted_def_t visit_sorted_def_attempt_sort;
+
+static visit_sorted_def_children_t visit_inplace_refs;
+
+
+
+/************** STATIC FUNCTIONS *******************/
+
 static int _get_def_i(compiler_t *compiler, type_def_t *def) {
+    /* Returns the index of def within compiler->defs.elems */
+
     for (int i = 0; i < compiler->defs.len; i++) {
         if (def == compiler->defs.elems[i]) return i;
     }
@@ -18,30 +93,84 @@ static int _get_def_i(compiler_t *compiler, type_def_t *def) {
     exit(1);
 }
 
-typedef struct sorted_def {
-    /* Information about a def which is being sorted by
-    compiler_sort_inplace_refs */
+static void sorted_def_sort(sorted_def_t *sorted_def) {
+    /* Push sorted_def's def, and mark sorted_def as having been sorted */
 
-    type_def_t *def;
+    sorter_t *sorter = sorted_def->sorter;
+    *(--sorter->new_defs_end) = sorted_def->def;
+    sorted_def->sorted = 1;
+}
 
-    int
-        /* has_inplace_refs: quick lookup for whether this def has any
-        inplace references to it from other defs */
-        has_inplace_refs: 1,
 
-        /* sorted: quick lookup for whether this def has been sorted
-        (assigned a new_i value) */
-        sorted: 1;
+/* visit_sorted_def_t */
+static void visit_sorted_def_mark_non_root(sorted_def_t *sorted_def) {
+    sorted_def->non_root = 1;
+}
 
-    /* Once this def has been sorted, new_i is the index it will have in
-    the new compiler->defs.elems */
-    size_t new_i;
-} sorted_def_t;
+/* visit_sorted_def_t */
+static void visit_sorted_def_attempt_sort(sorted_def_t *sorted_def) {
+    if (!sorted_def->sorted) {
+        /* Push sorted_def's def, and mark sorted_def as having been sorted */
+        sorted_def_sort(sorted_def);
+    }
+}
 
-int compiler_sort_inplace_refs(compiler_t *compiler) {
-    /* This function sorts compiler->defs.
-    In particular, defs are sorted to come after any defs they have an
-    inplace reference to (e.g. array subdef, struct/union field). */
+
+/* visit_sorted_def_children_t */
+static void visit_inplace_refs(compiler_t *compiler,
+    type_def_t *def, sorted_def_t *sorted_def,
+    sorted_def_t *sorted_defs, visit_sorted_def_t *visit_sorted_def
+) {
+    type_t *type = &def->type;
+    switch (type->tag) {
+        case TYPE_TAG_ARRAY: {
+            type_ref_t *ref = type->u.array_f.subtype_ref;
+            if (type_ref_is_inplace(ref)) {
+                type_def_t *subdef = type_get_def(&ref->type);
+                if (subdef) {
+                    sorted_def_t *sorted_def = &sorted_defs[
+                        _get_def_i(compiler, type_def_unalias(subdef))];
+                    visit_sorted_def(sorted_def);
+                }
+            }
+            break;
+        }
+        case TYPE_TAG_STRUCT: case TYPE_TAG_UNION: {
+            ARRAY_FOR(type_field_t, type->u.struct_f.fields, field) {
+                type_ref_t *ref = &field->ref;
+                if (type_ref_is_inplace(ref)) {
+                    type_def_t *subdef = type_get_def(&ref->type);
+                    if (subdef) {
+                        sorted_def_t *sorted_def = &sorted_defs[
+                            _get_def_i(compiler, type_def_unalias(subdef))];
+                        visit_sorted_def(sorted_def);
+                    }
+                }
+            }
+            break;
+        }
+        default: break;
+    }
+}
+
+
+static int compiler_sort_defs(compiler_t *compiler,
+    visit_sorted_def_children_t *visit_sorted_def_children
+) {
+    /* This function sorts compiler->defs by traversing them breadth-first,
+    and reordering them to match the order in which they were visited.
+
+    The function which defines the partial ordering is passed in as
+    visit_sorted_def_children, which recursively "visits" child defs of
+    the def passed to it.
+    It does not *call itself* recursively, rather it implements a
+    breadth-first traversal by pushing *children to be visited* onto a
+    queue (see sorter->new_defs_end, which is the queue, and sorted_def_sort,
+    which pushes defs onto it).
+
+    It is assumed that compiler->defs forms a partial ordering under
+    visit_sorted_def_children, or rather a forest of partial orderings (so,
+    with multiple roots). */
 
     /* Returned at end of function */
     int err = 0;
@@ -65,119 +194,59 @@ int compiler_sort_inplace_refs(compiler_t *compiler) {
         goto end;
     }
 
-    /* Populate sorted_defs */
+    sorter_t sorter = {
+
+        /* Used to push defs onto new_defs, starting at the right and going
+        leftwards, see sorted_def_sort */
+        .new_defs_end = new_defs + compiler->defs.len,
+
+    };
+
+    /* Populate sorted_defs, including figuring out which ones are the
+    root(s) of the partial ordering(s). */
     for (int i = 0; i < compiler->defs.len; i++) {
         type_def_t *def = compiler->defs.elems[i];
         sorted_def_t *sorted_def = &sorted_defs[i];
 
-        /* Populate sorted_def->def */
+        /* Populate sorted_def */
+        sorted_def->sorter = &sorter;
         sorted_def->def = def;
 
-        /* Populate other sorted_defs' has_inplace_refs, by following this
-        def's inplace refs */
-        type_t *type = &def->type;
-        switch (type->tag) {
-            case TYPE_TAG_ARRAY: {
-                type_ref_t *ref = type->u.array_f.subtype_ref;
-                if (type_ref_is_inplace(ref)) {
-                    type_def_t *subdef = type_get_def(&ref->type);
-                    if (subdef) {
-                        sorted_def_t *sorted_def = &sorted_defs[
-                            _get_def_i(compiler, type_def_unalias(subdef))];
-                        sorted_def->has_inplace_refs = 1;
-                    }
-                }
-                break;
-            }
-            case TYPE_TAG_STRUCT: case TYPE_TAG_UNION: {
-                ARRAY_FOR(type_field_t, type->u.struct_f.fields, field) {
-                    type_ref_t *ref = &field->ref;
-                    if (type_ref_is_inplace(ref)) {
-                        type_def_t *subdef = type_get_def(&ref->type);
-                        if (subdef) {
-                            sorted_def_t *sorted_def = &sorted_defs[
-                                _get_def_i(compiler, type_def_unalias(subdef))];
-                            sorted_def->has_inplace_refs = 1;
-                        }
-                    }
-                }
-                break;
-            }
-            default: break;
-        }
+        /* Populate other sorted_defs' sorted_def->non_root */
+        visit_sorted_def_children(compiler, def, sorted_def, sorted_defs,
+            &visit_sorted_def_mark_non_root);
     }
 
 
     /************** THE ALGORITHM *************/
-
-    /* Pointer to the end of new_defs, so that we can push defs onto the
-    end of it easily, using the expression: *(--new_defs_end) = def
-    NOTE: by "end" I mean... uhhh... the start. O_o
-    But we're treating new_defs as a stack which fills up from the right.
-    So new_defs_end is the *left* side of the stack, where things are pushed.
-    Do you see?.. */
-    type_def_t **new_defs_end = new_defs + compiler->defs.len;
 
     /* Push "root" defs (ones with no references to them) onto the new_defs
     queue */
     for (int i = compiler->defs.len - 1; i >= 0; i--) {
         type_def_t *def = compiler->defs.elems[i];
         sorted_def_t *sorted_def = &sorted_defs[_get_def_i(compiler, def)];
-        if (sorted_def->has_inplace_refs) continue;
+        if (sorted_def->non_root) continue;
 
-        /* Push def */
-        *(--new_defs_end) = def;
-        sorted_def->sorted = 1;
+        /* Push sorted_def's def, and mark sorted_def as having been sorted */
+        sorted_def_sort(sorted_def);
     }
 
-    /* Iterate over new_defs from the end, treating it as a queue, finding
-    each def's inplace refs and pushing their defs onto new_defs.
-    At the end of this process, new_defs should be completely populated. */
+    /* Iterate over new_defs from the end, treating it as a queue.
+    Each iteration represents "visiting" a def, and pushes its children
+    onto the new_defs queue, to be visited in a subsequent iteration.
+    At the end of this process, new_defs should be completely populated
+    (and will be in the reverse order of the breadth-first traversal). */
     for (int i = compiler->defs.len - 1; i >= 0; i--) {
         sorted_def_t *sorted_def = &sorted_defs[i];
         type_def_t *def = sorted_def->def;
 
-        /* Populate other sorted_defs' has_inplace_refs, by following this
+        /* Add defs to new_defs (using new_defs_end), by following this
         def's inplace refs */
-        type_t *type = &def->type;
-        switch (type->tag) {
-            case TYPE_TAG_ARRAY: {
-                type_ref_t *ref = type->u.array_f.subtype_ref;
-                if (type_ref_is_inplace(ref)) {
-                    type_def_t *subdef = type_get_def(&ref->type);
-                    if (subdef) {
-                        sorted_def_t *sorted_def = &sorted_defs[
-                            _get_def_i(compiler, type_def_unalias(subdef))];
-                        if (!sorted_def->sorted) {
-                            sorted_def->sorted = 1;
-                            *(--new_defs_end) = sorted_def->def;
-                        }
-                    }
-                }
-                break;
-            }
-            case TYPE_TAG_STRUCT: case TYPE_TAG_UNION: {
-                ARRAY_FOR(type_field_t, type->u.struct_f.fields, field) {
-                    type_ref_t *ref = &field->ref;
-                    if (type_ref_is_inplace(ref)) {
-                        type_def_t *subdef = type_get_def(&ref->type);
-                        if (subdef) {
-                            sorted_def_t *sorted_def = &sorted_defs[
-                                _get_def_i(compiler, type_def_unalias(subdef))];
-                            if (!sorted_def->sorted) {
-                                sorted_def->sorted = 1;
-                                *(--new_defs_end) = sorted_def->def;
-                            }
-                        }
-                    }
-                }
-                break;
-            }
-            default: break;
-        }
+        visit_sorted_def_children(compiler, def, sorted_def, sorted_defs,
+            &visit_sorted_def_attempt_sort);
     }
 
-    if (new_defs_end != new_defs) {
+    if (sorter.new_defs_end != new_defs) {
         /* This should never happen.
         If it does... maybe we want some better logging here, like... a list
         of all defs which *were* visited?..
@@ -185,7 +254,7 @@ int compiler_sort_inplace_refs(compiler_t *compiler) {
         fprintf(stderr,
             "Not all defs were visited during breadth-first traversal\n");
         fprintf(stderr, "(Missing %zu defs out of %zu)\n",
-            new_defs_end - new_defs,
+            sorter.new_defs_end - new_defs,
             compiler->defs.len);
         err = 2;
         goto end;
@@ -204,6 +273,20 @@ end:
     free(sorted_defs);
     free(new_defs);
     return err;
+}
+
+
+
+
+
+/************** PUBLIC FUNCTIONS *******************/
+
+int compiler_sort_inplace_refs(compiler_t *compiler) {
+    /* This function sorts compiler->defs such that each def comes after
+    any defs it has an inplace reference to (e.g. array subdef,
+    struct/union field). */
+
+    return compiler_sort_defs(compiler, &visit_inplace_refs);
 }
 
 static int _compare_aliases(const void *ptr1, const void *ptr2) {
@@ -241,7 +324,7 @@ int compiler_sort_aliases(compiler_t *compiler) {
     type_def_t **new_defs = calloc(compiler->defs.size, sizeof(*new_defs));
     if (!new_defs) return 1;
 
-    /* Copy all non-alias defs into new_defs */
+    /* Copy all non-alias defs into new_defs (before all the alias defs) */
     size_t new_defs_len = 0;
     ARRAY_FOR_PTR(type_def_t, compiler->defs, def) {
         if (def->type.tag == TYPE_TAG_ALIAS) continue;
